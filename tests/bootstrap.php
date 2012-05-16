4d4ef4623d2d1a6406f1d8cc767d6cfb09e26464
Augusto Pascutti <augusto@phpsp.org.br>
2012-05-15 23:59:54 -0300

    Try to fix tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,5 +3,6 @@ date_default_timezone_set('UTC');
 error_reporting(0);
 chdir(__DIR__.'/../');
 
+require 'lib/simple_html_dom.php';
 define('PATH_ROOT', realpath(__DIR__.'/../'));
 define('PATH_TEMPLATES', PATH_ROOT.'/tests/templates');
